f29dd2cce858c54c8846b0bc514e610f825853f5
co60 <co60@localhost.localdomain>
2024-06-02 16:17:34 +0200

    slightly better solution for day 2 part 1

--- 2015/2.2.py
+++ 2015/2.2.py
@@ -10,10 +10,11 @@ FILE: str = sys.argv[1]
 ribbon_sum: list[int] = []
 with open(FILE) as data:
     for _, line in enumerate(data):
-        dimentions: list[str] = line.split('x')
-        l: int = int(dimentions[0])
-        w: int = int(dimentions[1])
-        h: int = int(dimentions[2])
+        l: int = int(line.split('x')[0])
+        w: int = int(line.split('x')[1])
+        h: int = int(line.split('x')[2])
+        dimentions = line.split('x')
+        dimentions = [l,w,h]
         dimentions.sort()
         perimeter1 = dimentions[0] + dimentions[0]
         perimeter2 = dimentions[1] + dimentions[1]
