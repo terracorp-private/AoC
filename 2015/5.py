8145f7a0182f356a043fa60fb1408ab8a90d0167
terracorp-private <terracorp.private@gmail.com>
2024-06-05 03:14:00 +0200

    day 5 vowels parsing

--- 2015/5.py
+++ 2015/5.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 FILE: str = sys.argv[1]
 
+
+def parser(line) -> str:
+    vowels: str = r'[aeiou]'
+    res = re.findall(vowels, line)
+    if len(res) >= 3:
+        return res
+
+
 with open(FILE) as data:
-    line = data.readlines()
-    print(line)
+    lines = data.readlines()
+    vowels = [parser(line) for line in lines if parser(line) is not None]
+    print(vowels)
