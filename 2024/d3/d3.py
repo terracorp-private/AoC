26cf78b5207f11daf77f2187288a596ab2b56b87
terracorp-private <terracorp.private@gmail.com>
2024-12-15 13:56:28 +0100

    the most pathetic way to slove day 3 part 2

--- 2024/d3/d3.py
+++ 2024/d3/d3.py
@@ -1,36 +1,59 @@
 import sys
 import re
+import pandas as pd
 
 file = sys.argv[1]
 
 
-def multInstruction(lines):
+
+def multInstruction(data):
 
 
     resultSum = 0
     
-    for line in lines:
-        instructions = re.findall(r'mul\(\d*,\d*\)', line) 
+    instructions = re.findall(r'mul\(\d*,\d*\)', data) 
+   
+    for instruction in instructions:
+        
+        number = instruction.lstrip('mul(')
+        number = number.rstrip(r')')
+        firsNumber = int(number.split(',')[0])
+        secondNumber = int(number.split(',')[1])
 
-        for instruction in instructions:
-            
-            number = instruction.lstrip('mul(')
-            number = number.rstrip(r')')
-            firsNumber = int(number.split(',')[0])
-            secondNumber = int(number.split(',')[1])
-
-            resultProduct = firsNumber * secondNumber
-            resultSum = resultSum + resultProduct
-            
-            # print(instruction, number, firsNumber, secondNumber, sep='\t')
+        resultProduct = firsNumber * secondNumber
+        resultSum = resultSum + resultProduct
+        
 
     return resultSum
 
 
-with open(file) as data:
+def clearRAM(data):
+    
 
-    reportString = data.readlines()
+    instrList = re.findall(r'(don\'t\(\)|mul\(\d*,\d*\)|do\(\))', data)
+    mult = re.findall(r'mul\(\d*,\d*\)', data) 
+    do = re.findall(r'do\(\)', data) 
+    dont = re.findall(r'don\'t\(\)', data) 
    
-    print(multInstruction(reportString))
-     
+    return instrList
+        
+    
 
+def tunedInstr(data):
+
+    mult = re.findall(r'mul\(\d*,\d*\)', data) 
+    return mult
+
+
+with open(file) as f:
+
+
+    dataRaw = f.read()
+
+
+    print(multInstruction(dataRaw))
+    # df = pd.DataFrame({'Instrunctions': clearRAM(dataRaw)})
+    # df.to_csv('clearedData.csv', index=False)
+   
+
+# answer_part2 = 113965544
